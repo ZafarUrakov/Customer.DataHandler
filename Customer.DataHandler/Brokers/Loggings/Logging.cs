7d6c88f8773c7f728fc78b6e429139ad25fc7890
Zafar <icom@MACKBOOK>
2023-09-27 11:17:21 +0300

    BROKERS:  Logging Register

---
+++ Customer.DataHandler/Brokers/Loggings/Logging.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Customer.DataHandler.Data.Loggings
+{
+    internal class Logging
+    {
+        internal void LoggingError(Exception exception) =>
+            Console.WriteLine(exception.Message);
+    }
+}
