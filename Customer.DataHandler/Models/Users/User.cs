80e41560002c4d222fd69df6c439772611ae9c82
Zafar <icom@MACKBOOK>
2023-09-27 11:00:11 +0300

    DATA: Create Model

---
+++ Customer.DataHandler/Models/Users/User.cs
@@ -0,0 +1,32 @@
+﻿//=================================
+// Copyright (c) Customer LLC.
+// Powering True Leadership
+//===============================
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Customer.DataHandler.Models.Users
+{
+    internal class User
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Firstname - Text is required")]
+        public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "Lastname - Text is required")]
+        public string Lastname { get; set; }
+
+        [Required(ErrorMessage = "BirthDate - Date is required")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "PhoneNumber - Text is required")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Email - Text is required")]
+        [EmailAddress(ErrorMessage = "Email - Email is invalid")]
+        public string Email { get; set; }
+    }
+}
