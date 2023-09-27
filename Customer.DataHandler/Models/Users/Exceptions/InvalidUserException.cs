58616083cfbc92502a0bac0f65ecfb9aa0d045f6
Zafar <icom@MACKBOOK>
2023-09-27 11:20:23 +0300

    POC: User Add(WIP)

---
+++ Customer.DataHandler/Models/Users/Exceptions/InvalidUserException.cs
@@ -0,0 +1,16 @@
+﻿//=================================
+// Copyright (c) Customer LLC.
+// Powering True Leadership
+//===============================
+
+using System;
+
+namespace Customer.DataHandler.Models.Users.Exceptions
+{
+    internal class InvalidUserException : Exception
+    {
+        public InvalidUserException()
+            : base(message: "Client is invalide. Fix the errors and try again.")
+        { }
+    }
+}
---
+++ Customer.DataHandler/Models/Users/UserValidator.cs
@@ -0,0 +1,21 @@
+﻿//=================================
+// Copyright (c) Sofee LLC.
+// Powering True Leadership
+//===============================
+
+using FluentValidation;
+
+namespace Customer.DataHandler.Models.Users
+{
+    internal class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(client => client.Id).NotEmpty().WithMessage("Id - Id is required");
+            RuleFor(client => client.Firstname).NotEmpty().WithMessage("Firstname - Text is required");
+            RuleFor(client => client.Lastname).NotEmpty().WithMessage("Lastname - Text is required");
+            RuleFor(client => client.BirthDate).NotEmpty().WithMessage("BirthDate - Date is required");
+            RuleFor(client => client.Email).NotEmpty().WithMessage("Email - Text is required").EmailAddress().WithMessage("Email - Email is invalid");
+        }
+    }
+}
