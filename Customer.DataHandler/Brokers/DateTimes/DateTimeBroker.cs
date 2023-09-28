470a69c7ff94a666c96b1750bb06b99c7ecb25f2
Zafar <icom@MACKBOOK>
2023-09-28 10:34:49 +0300

    POC: Initialize Date Time Broker

---
+++ Customer.DataHandler/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,15 @@
+﻿//=================================
+// Copyright (c) Customer LLC.
+// Powering True Leadership
+//===============================
+
+using System;
+
+namespace Customer.DataHandler.Brokers.DateTimes
+{
+    internal class DateTimeBroker
+    {
+        internal DateTimeOffset GetDataTimeOffset() =>
+            DateTimeOffset.UtcNow;
+    }
+}
